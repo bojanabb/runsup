a8f8ec36ac5b6e9c9ec73fa0ee726a8e6f38b88c
bojanaa <bulatovicbojana2@gmail.com>
2019-03-30 15:08:45 +0100

    initial commit

--- app/src/main/java/si/uni_lj/fri/pbd2019/runsup2/MapsActivity.java
+++ app/src/main/java/si/uni_lj/fri/pbd2019/runsup2/MapsActivity.java
@@ -20,7 +20,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+                .findFragmentById(R.id.fragment_maps_workoutmap);
         mapFragment.getMapAsync(this);
     }
 
@@ -39,8 +39,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng ljubljana = new LatLng(46.056946, 14.505751);
+        mMap.addMarker(new MarkerOptions().position(ljubljana).title("Marker in Ljubljana"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(ljubljana));
     }
 }
