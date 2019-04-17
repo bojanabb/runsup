1129c0708af1bca5cdc8536d60e820574d2b18e5
bojanaa <bulatovicbojana2@gmail.com>
2019-04-17 17:58:05 +0200

    initial commit

--- app/src/main/java/si/uni_lj/fri/pbd2019/runsup/StopwatchActivity.java
+++ app/src/main/java/si/uni_lj/fri/pbd2019/runsup/StopwatchActivity.java
@@ -1,12 +1,16 @@
 package si.uni_lj.fri.pbd2019.runsup;
 
 
+import android.Manifest;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -23,14 +27,18 @@ public class StopwatchActivity extends AppCompatActivity {
     boolean firstClick=true;
     int tempF=1;
     MyBroadCastReceiver mBroadcastReceiver;
+    private static final int REQUEST_ID_LOCATION_PERMISSIONS = 34;
+    private final String TAG = "MainActivity";
     public final static String TICK  = "si.uni_lj.fri.pbd2019.runsup2.TICK";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stopwatch);
-
-                    final Button button = (Button) findViewById(R.id.button_stopwatch_start);
+        Intent intent = new Intent(StopwatchActivity.this, TrackerService.class);
+        startService(intent);
+        startLocationUpdates();
+        final Button button = (Button) findViewById(R.id.button_stopwatch_start);
                     final Button button1 = (Button) findViewById(R.id.button_stopwatch_endworkout);
         mBroadcastReceiver = new MyBroadCastReceiver();
                     button.setOnClickListener(new View.OnClickListener() {
@@ -38,6 +46,7 @@ public class StopwatchActivity extends AppCompatActivity {
                         public void onClick(View v) {
                             if(firstClick==true) {
                                 if(tempF==1) {
+                                    Log.d("Heelp", "aaa");
                                     Intent intent = new Intent(StopwatchActivity.this, TrackerService.class);
                                     intent.setAction(TrackerService.COMMAND_START);
                                     startService(intent);
@@ -128,5 +137,46 @@ public class StopwatchActivity extends AppCompatActivity {
         Intent intent = new Intent(StopwatchActivity.this, TrackerService.class);
         stopService(intent);
         super.onDestroy();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        if (requestCode == REQUEST_ID_LOCATION_PERMISSIONS) {
+            Log.d("MainActivity",String.valueOf(grantResults.length));
+            if (grantResults.length > 0
+                    && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                startLocationUpdates();
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    private void startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "Asking for permission");
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission
+                    .ACCESS_COARSE_LOCATION) || ActivityCompat.shouldShowRequestPermissionRationale (this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)){
+                Snackbar.make(findViewById(R.id.constraintlayout_stopwatch_content), R.string.permission_location_rationale,
+                        Snackbar.LENGTH_INDEFINITE)
+                        .setAction(R.string.ok, new View.OnClickListener() {
+
+                            @Override
+                            public void onClick(View view) {
+                                ActivityCompat.requestPermissions(StopwatchActivity.this,
+                                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,
+                                                Manifest.permission.ACCESS_FINE_LOCATION},
+                                        REQUEST_ID_LOCATION_PERMISSIONS);
+                            }
+                        }).show();
+            }
+            else{
+                ActivityCompat.requestPermissions(StopwatchActivity.this,
+                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,
+                                Manifest.permission.ACCESS_FINE_LOCATION},
+                        REQUEST_ID_LOCATION_PERMISSIONS);
+            }
+
+        }
     }
 }
--- app/src/main/java/si/uni_lj/fri/pbd2019/runsup/services/TrackerService.java
+++ app/src/main/java/si/uni_lj/fri/pbd2019/runsup/services/TrackerService.java
@@ -1,17 +1,26 @@
 package si.uni_lj.fri.pbd2019.runsup.services;
 
+import android.Manifest;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Binder;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.SystemClock;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 import java.util.ArrayList;
 import java.util.Timer;
@@ -25,7 +34,12 @@ public class TrackerService extends Service {
     private LocationRequest mLocationRequest;
     private LocationCallback mLocationCallback;
     private Location mCurrentLocation;
+    LocationManager mLocationManager;
     public long duration=0;
+    //int sportType = 0;
+    int mState=0;
+
+    float distance=0;
     Boolean mRequestingLocationUpdates = false;
     String mLastUpdateTime;
     ArrayList<Location> locationList;
@@ -81,26 +95,88 @@ public class TrackerService extends Service {
     @Override
     public void onCreate() {
         handler=new Handler();
+        locationList = new ArrayList<Location>();
         Log.d("MainActivity","Service has started");
         super.onCreate();
     }
 
+
+    LocationListener locationListenerGPS=new LocationListener() {
+        @Override
+        public void onLocationChanged(android.location.Location location) {
+            double lat=location.getLatitude();
+            double lon=location.getLongitude();
+            Log.d("MainLoc", String.valueOf(lat));
+            Log.d("MainLoc", String.valueOf(lon));
+            int size=locationList.size();
+            if(mState==2){
+                distance=distance+locationList.get(size-1).distanceTo(location);
+                if(distance<=100){
+                    locationList.add(location);
+                }
+            }
+            else {
+                if (size != 0) {
+                    distance = distance + locationList.get(size - 1).distanceTo(location);
+                }
+                locationList.add(location);
+            }
+
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
+
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         if(intent.getAction()==COMMAND_START) {
+           // sportType=intent.getExtras().getInt("sportType");
+            mState=0;
+            mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                mFusedLocationProviderClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if(location != null) {
+                            locationList.add(location);
+                        }
+                    }
+                });
+            }
+            Log.d("TrackerCheck", "in command start");
             start = SystemClock.uptimeMillis();
+            mLocationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+            if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            }
+            mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 3000, 10, locationListenerGPS);
             mLastUpdateTime = "";
             timeTick();
         }
         else if(intent.getAction()==COMMAND_PAUSE){
             this.duration =(SystemClock.uptimeMillis()	-start);
+            mState=1;
             handler.removeMessages(0);
         }
         else if(intent.getAction()==COMMAND_CONTINUE) {
             start=SystemClock.uptimeMillis()-duration;
+            mState=2;
             timeTick();
         }
         else if(intent.getAction()==COMMAND_STOP) {
+            mState=3;
             handler.removeMessages(0);
             stopSelf();
         }
@@ -108,21 +184,19 @@ public class TrackerService extends Service {
     }
 
     public int getState() {
-        return 0;
+        return mState;
     }
 
     public long getDuration() {
         return duration;
     }
 
-    public long getDistance() {
-        long dist=0;
-         return dist;
+    public double getDistance() {
+        return distance;
     }
 
-    public long getPace() {
-        long pac=0;
-        return pac;
+    public double getPace() {
+        return (getDuration()/60)/(getDistance()/1000);
     }
 
     public TrackerService() {
@@ -136,6 +210,20 @@ public class TrackerService extends Service {
     public class LocalBinder extends Binder {
         public TrackerService getService() {
             return TrackerService.this;
+        }
+    }
+
+    public void printLocations(){
+        int len=locationList.size();
+        Log.d("MainActivityLen",String.valueOf(len));
+        if(len>0) {
+            for (int i = 1; i <= len; i++) {
+                double lat = locationList.get(i).getLatitude();
+                double lon = locationList.get(i).getLongitude();
+                Log.d("MainLoc", String.valueOf(lat));
+                Log.d("MainLoc", String.valueOf(lon));
+
+            }
         }
     }
 }
