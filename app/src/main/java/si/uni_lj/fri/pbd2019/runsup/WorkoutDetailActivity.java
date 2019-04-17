2d90bb3da2838322e942e0ea3eda2c65e52fe013
bojanaa <bulatovicbojana2@gmail.com>
2019-04-17 13:00:14 +0200

    initial commit

--- app/src/main/java/si/uni_lj/fri/pbd2019/runsup/WorkoutDetailActivity.java
+++ app/src/main/java/si/uni_lj/fri/pbd2019/runsup/WorkoutDetailActivity.java
@@ -1,10 +1,12 @@
 package si.uni_lj.fri.pbd2019.runsup;
 import android.content.Intent;
 import android.location.Location;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,22 +19,11 @@ public class WorkoutDetailActivity extends AppCompatActivity {
     double pace=0;
     double calories=0;
     ArrayList<List<Location>> finalPositionList;
-//     TextView titleTV = (TextView)findViewById(R.id.textview_workoutdetail_workouttitle);
-//    TextView SportAct = (TextView)findViewById(R.id.textview_workoutdetail_sportactivity);
-//    TextView DateAct = (TextView)findViewById(R.id.textview_workoutdetail_activitydate);
-//    TextView ValDuration = (TextView)findViewById(R.id.textview_workoutdetail_valueduration);
-//    TextView ValCalories = (TextView)findViewById(R.id.textview_workoutdetail_valuecalories);
-//    TextView ValDistance = (TextView)findViewById(R.id.textview_workoutdetail_valuedistance);
-//        String titleTV = getResources().getString(R.string.workoutdetail_workoutname);
-//        String SportAct = getResources().getString(R.string.textview_workoutdetail_sportactivity);
-//        String DateAct = getResources().getString(R.string.textview_workoutdetail_activitydate);
-//        String ValDur = getResources().getString(R.string.textview_workoutdetail_valueduration);
-//        String ValCal = getResources().getString(R.string.textview_workoutdetail_valuecalories);
-//        String ValDist = getResources().getString(R.string.textview_workoutdetail_valuedistance);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        EditText sharemes = (EditText)findViewById(R.id.share_m)
         setContentView(R.layout.activity_workout_detail);
         final Button buttonn = (Button) findViewById(R.id.button_workoutdetail_showmap);
         buttonn.setOnClickListener(new View.OnClickListener() {
@@ -40,6 +31,18 @@ public class WorkoutDetailActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(WorkoutDetailActivity.this, MapsActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        final Button email = (Button) findViewById(R.id.button_workoutdetail_emailshare);
+        final Button facebook = (Button) findViewById(R.id.button_workoutdetail_fbsharebtn);
+        final Button twitter = (Button) findViewById(R.id.button_workoutdetail_twittershare);
+        email.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent eIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto","", null ));
+                eIntent.putExtra(Intent.EXTRA_SUBJECT, "Workout summary");
+//                eIntent.putExtra(Intent.EXTRA_TEXT, sha)
             }
         });
     }
